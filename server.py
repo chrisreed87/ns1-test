2a8ed0caa0f18978faeb90b5e0b41c9b9d941c8d
Chris Reed <chrisreed87@gmail.com>
2020-07-30 21:36:36 -0700

    todos

--- server.py
+++ server.py
@@ -1,31 +1,26 @@
-from flask import Flask, jsonify, request, abort
-from flask_hashing import Hashing
-from prometheus_flask_exporter import PrometheusMetrics
-
-
-
-app = Flask(__name__)
-hashing = Hashing(app)
-metrics = PrometheusMetrics(app)
 # [X] 1. accepts a POST to /messages, contents of the message should be a string
-# [X] 2. response to the POST should be a SHA like
-'''
-{
-
-   “digest” : “bdfcba37390f1dc3d871011777098dab32c8dd9542b56291268ed950c8b58ba7”
-
-}
-'''
+# [X] 2. response to the POST should be a SHA 
 # [] 3. a get to /messages/<SHA> should return the message
 # [X] 4. /metrics should return some kind of metrics
 # [X] 5. Wrap it in a Dockerfile
 # [] 6. README how to run
 
+
+from flask import Flask, jsonify, request, abort
+from flask_hashing import Hashing
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+hashing = Hashing(app)
+metrics = PrometheusMetrics(app)
+
 db = []
+
 
 @app.route('/health')
 def hello_world():
     return 'OK'
+
 
 @app.route('/messages', methods=['POST'])
 def post_messages():
@@ -38,8 +33,7 @@ def post_messages():
   return jsonify({'digest': val_hash}), 201
 
 
-
-@app.route('/messages', methods=['GET'])
+@app.route('/messages', methods=['GET']) #TODO remove this endpoint when done debugging
 def get_db():
   return jsonify({'db': db})
 
@@ -48,7 +42,6 @@ def get_db():
 def get_messages(db_id):
   message = [message for message in db if message['id'] == db_id]
   return message[0] #TODO need to figure out how to filter just the message piece
-
 
 
 if __name__ == '__main__':
